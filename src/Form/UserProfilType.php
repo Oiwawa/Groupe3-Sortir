748487530eb74d6bcac23a4042f1ac68ae0a2046
Oiwawa <78601126+Oiwawa@users.noreply.github.com>
2021-04-06 21:03:18 +0200

    commit liens navbar

--- src/Form/UserProfilType.php
+++ src/Form/UserProfilType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +16,13 @@ class UserProfilType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', null,['label'=>'Pseudo: '])
-            ->add('firstName',null,['label'=>'Prénom: '])
-            ->add('lastName',null,['label'=>'Nom: '])
-            ->add('phone',null,['label'=>'Téléphone: '])
-            ->add('mail', null,['label'=>'Email: '])
-            ->add('password',null,['label'=>'Mot de passe: '])
-            ->add('password',null,['label'=>'Confirmation: '])
+            ->add('username', TextType::class,['label'=>'Pseudo: ', 'empty_data'=>'app.user.username'])
+            ->add('firstName',TextType::class,['label'=>'Prénom: '])
+            ->add('lastName',TextType::class,['label'=>'Nom: '])
+            ->add('phone',TextType::class,['label'=>'Téléphone: '])
+            ->add('mail', TextType::class,['label'=>'Email: '])
+            ->add('password', PasswordType::class,['label'=>'Mot de passe: '],)
+            ->add('password', RepeatedType::class,['label'=>'Confirmation: '])
 //            ->add('campus',ChoiceType::class, ['choices' =>['Saint-Herblain'=>'Saint-Herblain',
 //                'Nantes'=>'Nantes', 'La-Roche-sur-Yon'=>'La-Roche-sur-Yon']])
 //            ->add('Ma photo')
